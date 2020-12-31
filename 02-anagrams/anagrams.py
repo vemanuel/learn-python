ceee10c3b55d9d2f203cf3697f6fff1e31365f39
Victor M. Emanuel <victor.m.emanuel@gmail.com>
2020-12-31 12:19:14 -0500

    anagrams.py - loading dictionary to anagram table in progress. lunch break.

--- 02-anagrams/anagrams.py
+++ 02-anagrams/anagrams.py
@@ -10,15 +10,48 @@ Victor M. Emanuel
 # *****************************************************************************
 def get_input():
     """Prompt user for input."""
-    letters = input('Enter letters:\n')
+    letters = input('Enter letters, Enter to quit:\n')
     return letters
+
+# *****************************************************************************
+def load_words():
+    """
+    Load scrabble dictionary and create anagrams dict with
+    key= sorted letters, value= list of anagrams
+    Example:
+        {
+            'act': ['act', 'cat', 'tac']
+            'dgo': ['dog', 'god'],
+        }
+    """
+    # Load all the words from the scrabble dictionary into a python list, words
+    fname = 'words.txt'
+    with open(fname) as fh:
+        words = fh.readlines()
+    
+    # Create a python dict keyed by sorted letters, with value equal to a list
+    # of all the anagrams of that collection of letters
+    anagrams = dict()
+    for word in words:
+        pass
+    return anagrams
 
 # *****************************************************************************
 def main():
     """Console program."""
-    letters = get_input()
-    print(f'You chose:\n{letters}')
-
+    # Load the anagrams dictionary
+    anagrams = load_words()
+    
+    # Keep processing input from the user until they decide to quit
+    while True:
+        letters = get_input()
+        if len(letters) > 0:
+            print(f'You chose:\n{letters}\n')
+        else:
+            quit_prompt = input('Quit? Enter to confirm.')
+            if len(quit_prompt) == 0:
+                break
+    
 # *****************************************************************************
 if __name__ == '__main__':
     main()
